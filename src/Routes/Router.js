6a972ff36934dda9961a74181c5760a865f0e3d0
Pankajkumar90 <pankajbaliyan90@gmail.com>
2023-07-06 07:46:37 +0530

    add notes

--- src/Routes/Router.js
+++ src/Routes/Router.js
@@ -3,12 +3,14 @@ import Home from '../components/Home';
 import Navbar from '../components/Navbar';
 import About from '../components/About';
 import NoteState from '../context/notes/NoteState';
+import Alert from '../components/Alert';
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <NoteState>
                 <Navbar />
+                <Alert message="Hello World" />
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/about" element={<About />} />
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -2,21 +2,44 @@ import React, { useState } from 'react';
 import NoteContext from './noteContext';
 
 const NoteState = (props) => {
-    const s1 = {
-        name: 'Pankaj',
-        class: 'MCA',
+    const notesInitial = [
+        {
+            _id: '649fc358aa274bff77e77dca',
+            user: '649fbdfad33887cd06ca0953',
+            title: 'MY title',
+            description: 'Jaata ka choora',
+            tag: 'Jaat',
+            date: '2023-07-01T06:10:32.527Z',
+            __v: 0,
+        },
+    ];
+
+    const [notes, setNotes] = useState(notesInitial);
+
+    //Add a note
+    const addNote = (title, description, tag) => {
+        const note = {
+            _id: '64a2acd92cbc3c924da6c65f',
+            user: '649fbdfad33887cd06ca0953',
+            title: title,
+            description: description,
+            tag: tag,
+            date: '2023-07-03T11:11:21.069Z',
+            __v: 0,
+        };
+        setNotes(notes.concat(note));
     };
-    const [state, setState] = useState(s1);
-    const update = () => {
-        setTimeout(() => {
-            setState({
-                name: 'Harry',
-                class: 'BCA',
-            });
-        }, 3000);
-    };
+
+    //Delete a note
+    const deleteNote = () => {};
+
+    //Edit a note
+    const updateNote = () => {};
+
     return (
-        <NoteContext.Provider value={{ state, update }}>
+        <NoteContext.Provider
+            value={{ notes, addNote, deleteNote, updateNote }}
+        >
             {props.children}
         </NoteContext.Provider>
     );
